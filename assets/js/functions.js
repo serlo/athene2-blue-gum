e25d5ab4dfb2d88c5334fcb003fe0412d8d03cbc
elpoto <1258870+elbotho@users.noreply.github.com>
2018-08-02 14:41:24 +0200

    full jQuery, smooth scroll, nav dropdown

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,4 +7,26 @@ $( document ).ready(function() {
 		$('body').removeClass('overlay-menu');
 	});
 
+
+	$('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+	        || location.hostname == this.hostname) {
+
+	        var target = $(this.hash);
+	        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	           if (target.length) {
+	           	$('body').addClass("no-events");
+	           	var distance = Math.abs($(document).scrollTop() - target.offset().top);
+				var time = distance / 18 + 250;
+				if(time>800) time = 800;
+	             $('html,body').animate({
+	                 scrollTop: target.offset().top
+	            }, time, function(){
+	            	$('body').removeClass("no-events");
+	            });
+	            return false;
+	        }
+	    }
+	});
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var gulp = require('gulp'),
 
 gulp.task("concatScripts", function() {
 	return gulp.src([
-		'assets/js/vendor/jquery-3.2.1.slim.min.js',
+		'assets/js/vendor/jquery-3.2.1.min.js',
 		'assets/js/vendor/popper.min.js',
 		'assets/js/vendor/bootstrap.min.js',
 		'assets/js/functions.js'
