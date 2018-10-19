93cf26d0f8ff4f8712f0f7cb6d24057ed1a60949
Jonas Keinholz <jonas@keinholz.com>
2018-10-19 16:42:19 +0200

    feat: topnav

--- react/.storybook/webpack.config.js
+++ react/.storybook/webpack.config.js
@@ -5,6 +5,15 @@ module.exports = (_baseConfig, _env, defaultConfig) => {
       exclude: /node_modules/,
       loader: require.resolve('babel-loader')
     },
+    {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      loader: [
+        'style-loader', // creates style nodes from JS strings
+        'css-loader', // translates CSS into CommonJS
+        'sass-loader' // compiles Sass to CSS, using Node Sass by default
+      ]
+    },
     ...defaultConfig.module.rules.slice(1)
   ]
   defaultConfig.resolve.extensions.unshift('.ts', '.tsx')
