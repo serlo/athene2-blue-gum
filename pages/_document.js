1a809ca12e566bb0aacbc544b81daf8cf2a63b78
David Li <dawei.li@gmx.net>
2019-06-25 20:53:48 +0200

    prepare: customizing nextjs output to only render component (without enclosing html/head tags)

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -25,5 +25,37 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <html>
+        <head>
+          <meta charSet="utf-8" class="next-head" />
+          <meta
+            name="viewport"
+            content="width=device-width,minimum-scale=1,initial-scale=1"
+            class="next-head"
+          />
+        </head>
+        <body>
+          <MyScripts />
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+class MyScripts extends Head {
+  render() {
+    return (
+      <>
+        {this.props.children}
+        {this.getCssLinks()}
+        {this.context._documentProps.styles || null}
+      </>
+    )
   }
 }
