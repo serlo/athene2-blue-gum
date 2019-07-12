8643e00532af0dcf1b32bacebefc5c0a8bf6027f
David Li <dawei.li@gmx.net>
2019-07-12 10:52:51 +0200

    add example with POST

--- pages/__footer.tsx
+++ pages/__footer.tsx
@@ -22,6 +22,22 @@ export default function Index(props: any) {
 }
 Index.getInitialProps = async ({ req }: {req:any}) => {
   if (req) {
+    // read req body and parse json EXAMPLE
+    let data:any = await new Promise(res => {
+      let data:any = []
+      req.on('data', (chunk:any) => {
+        data.push(chunk)
+      })
+      req.on('end', () => {
+        res(JSON.parse(data))
+      })
+    })
+    console.log(data)
+    // use it with:
+    // curl -X POST -H 'Content-Type: application/json' -d '{"name": "New item", "year": "2009"}' localhost:3000/__footer
+    
+    
+    
     // the server can access request object and extract props
     const urlparams = new URLSearchParams(req.url.replace("/__footer?",""))
     return { footerNavEntries, serloSlogan:urlparams.has("slogan")?urlparams.get("slogan"):serloSlogan    }
